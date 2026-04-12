1c12653a1256c972f2fa0a252c8dfceab36c49b8
MohamedNasr <mohamednasr27122@gmail.com>
2026-04-12 14:32:13 +0200

    Add project files.

---
+++ EfCore/ApplicationDBcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore
+{
+    internal class ApplicationDBcontext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;DataBase=EFCore;Trusted_Connection=true;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ EfCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+        }
+    }
+}
